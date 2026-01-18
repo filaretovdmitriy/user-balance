0fb4c1af1aeea54d86f1a5b3d6347d75f435f3c7
dmitriyfilaretov <filaretovd@yandex.ru>
2026-01-18 14:46:00 +0300

    feat: api with last 5 transactions

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\TransactionsResource;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    public function latestTransactions(Request $request)
+    {
+        $user = $request->user();
+
+        $transactions = $user->transactions->latest()->take(5)->get();
+
+        return TransactionsResource::collection($transactions);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,5 @@ Route::get('/user', function (Request $request) {
 Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->get('/transactions/latest', [TransactionController::class, 'latestTransactions']);
